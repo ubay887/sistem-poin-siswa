74a994056186112f53b61a22cf13d695001275cb
Akhmad Herdian <akhmad.herdian@gmail.com>
2020-02-28 07:39:21 +0800

    Add level class for class_name

---
+++ app/Entities/Classes/Level.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Entities\Classes;
+
+/**
+ * Level Class
+ */
+class Level
+{
+    public static $lists = [
+        1  => 'I',
+        2  => 'II',
+        3  => 'III',
+        4  => 'IV',
+        5  => 'V',
+        6  => 'VI',
+        7  => 'VII',
+        8  => 'VIII',
+        9  => 'IX',
+        10 => 'X',
+        11 => 'XI',
+        12 => 'XII',
+    ];
+}
